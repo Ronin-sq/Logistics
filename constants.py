8fb1f41fa135ffb81b1445a82181a9d93a1373a9
tqt666 <1598235944@qq.com>
2024-10-01 16:26:06 +0800

    相机矫正，二维码识别

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ pwm_max = 7200
 pwm_min = 300
 flag = False
 color_config = {
-    1:[np.array([26, 128, 46]),np.array([34, 255, 255])],  # yellow
+    1:[np.array([0, 43, 46]),np.array([12, 255, 255])],  # yellow
     2:[np.array([35,43,46]),np.array([77,255,255])],    # green
     3:[np.array([100,43,46]),np.array([124,255,255])]  # blue
 }
--- identify.py
+++ identify.py
@@ -8,58 +8,74 @@ class VideoCapture:
     def __init__(self,index):
         self.frame = None
         self.video = cv2.VideoCapture(index)
+        self.K = np.array(
+            [[366.63195575538515, 0.0, 323.0034899844723], [0.0, 490.40727832499107, 213.25799648860593],
+             [0.0, 0.0, 1.0]])
+        self.D = np.array([[-0.14827706218149106], [0.10272564498472946], [-0.144721204897414], [0.06967408567430872]])
+        self.width, self.height = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH)), int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.p = cv2.fisheye.estimateNewCameraMatrixForUndistortRectify(self.K, self.D, (self.width, self.height), None)
+        self.mapx2, self.mapy2 = cv2.fisheye.initUndistortRectifyMap(self.K, self.D, None, self.p, (self.width, self.height), cv2.CV_32F)
+
+        self.frame = None
+        self.video = cv2.VideoCapture(index)
         self.video.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
         if not self.video.isOpened():
             print("无法打开摄像头")
             exit()
             
     def get_frame(self):
-        while True:
-            ret, self.frame = self.video.read()
-            if not ret:
-                print("无法接收帧 (stream end?)")
-                break
+        ret, self.frame = self.video.read()
+
+        self.frame = cv2.remap(self.frame, self.mapx2, self.mapy2, interpolation=cv2.INTER_LINEAR, borderMode=cv2.BORDER_CONSTANT)
+        if not ret:
+            print("无法接收帧 (stream end?)")
+        return self.frame
             
             
     def QR_code(self):
         qr_coder = cv2.QRCodeDetector()
-        while True:
-            try:
-                frame = self.get_frame()
-                # qr检测并解码
-                codeinfo, points, straight_qrcode = qr_coder.detectAndDecode(frame)
-                break
-            except:
-                print("检测二维码失败")
+        if 1:
+        #try:
+            frame = self.frame
+            # qr检测并解码
+            codeinfo, points, straight_qrcode = qr_coder.detectAndDecode(frame)
+
+
         # 绘制qr的检测结果
-        cv2.drawContours(frame, [np.int32(points)], 0, (0, 0, 255), 2)
-        print(points)
-        # 打印解码结果
-        print("qrcode :", codeinfo)
-        return codeinfo
+            if codeinfo !='' :
+                print(points)
+                # 打印解码结果
+                print("qrcode :", codeinfo)
+                return codeinfo
+        #except:
+            #print("检测二维码失败")
+
+
+
     
     def find_material(self, list):
         for i,element in enumerate(list):
-            while True:
                 frame = self.get_frame()
                 src = frame.copy()
-                kernel = np.ones((5,5),np.unit8)
+                kernel = (5, 5)
+
                 hsv = cv2.cvtColor(frame,cv2.COLOR_BGR2HSV)         # 转HSV通道
-                res = src.copy()  
-                mask = cv2.inRange(hsv, constants.color_config[element][0], constants.color_config[element][1])
-                #与函数，起到将两张黑色区域合并的效果  
-                res = cv2.bitwise_and(src, src, mask=mask)  
-                h,w = res.shape[:2]  
-                #滤波函数，滤除噪点：  
-                blured = cv2.blur(res,(5,5))  
-                ret, bright = cv2.threshold(blured,10,255,cv2.THRESH_BINARY)  
-                gray = cv2.cvtColor(bright,cv2.COLOR_BGR2GRAY)  
-                #开闭运算函数，滤除噪点，使得集合区域更加平滑  
-                kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel)  
-                opened = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)  
+                res = src.copy()
+                mask = cv2.inRange(hsv, constants.color_config[2][0], constants.color_config[2][1])
+                #与函数，起到将两张黑色区域合并的效果
+                res = cv2.bitwise_and(src, src, mask=mask)
+                h,w = res.shape[:2]
+                #滤波函数，滤除噪点：
+                blured = cv2.blur(res,(5,5))
+                ret, bright = cv2.threshold(blured,10,255,cv2.THRESH_BINARY)
+                gray = cv2.cvtColor(bright,cv2.COLOR_BGR2GRAY)
+                #开闭运算函数，滤除噪点，使得集合区域更加平滑
+                kernel = cv2.getStructuringElement(cv2.MORPH_RECT, kernel)
+                opened = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)
                 closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)
                 circles = cv2.HoughCircles(closed, cv2.HOUGH_GRADIENT, 1, 30, param1=100, param2=20, minRadius=5, maxRadius=150)
                 if circles is not None:  # 如果识别出圆
+
                     for circle in circles[0]:
                         #  获取圆的坐标与半径
                         x = int(circle[0])
@@ -67,19 +83,18 @@ class VideoCapture:
                         r = int(circle[2])
                         cv2.circle(frame, (x, y), r, (0, 255, 0), 3)  # 标记圆
                         cv2.circle(frame, (x, y), 6, (255, 255, 0), -1)  # 标记圆心
+                        print(f"x:{x},y:{y}")
                         return [x,y,r]
                 else:
-                    print("未识别到圆")
+                    #print("未识别到圆")
                     continue
     def cv_imshow(self):
-        while True:
-            cv2.imshow("frame", self.frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):  # 按 'q' 键关闭窗口
-                break
+        cv2.imshow("frame", self.frame)
+
 
     def release(self):
-        cv2.release()
         cv2.destroyAllWindows()
+        self.video.release()
 
 
 def get_text(list):
@@ -122,24 +137,29 @@ def show_mission(list):
     
     
 if __name__ == "__main__":
-    # # show_mission([1,2,3])
-    # video = VideoCapture(0)
-    # video.get_frame()
-    # video.cv_imshow()
-    # video.release()
-    cap = cv2.VideoCapture(0)
-    cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
-    if not cap.isOpened():
-        print("无法打开摄像头")
-        exit()
+    # show_mission([1,2,3])
+    video = VideoCapture(0)
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            print("无法接收帧 (stream end?)")
-            break
-        cv2.imshow('frame', frame)
+        video.find_material([1,2,3])
+        video.cv_imshow()
+
+        #video.cv_imshow()
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
-
-    cap.release()
-    cv2.destroyAllWindows()
+    video.release()
+    # cap = cv2.VideoCapture(0)
+    # #cap.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('M', 'J', 'P', 'G'))
+    # if not cap.isOpened():
+    #     print("无法打开摄像头")
+    #     exit()
+    # while True:
+    #     ret, frame = cap.read()
+    #     if not ret:
+    #         print("无法接收帧 (stream end?)")
+    #         break
+    #     cv2.imshow('frame', frame)
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    #
+    # cap.release()
+    # cv2.destroyAllWindows()
